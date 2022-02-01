192f0dc4fb1d87d81a0a8ab19d35ae31d75adae3
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-01 23:22:05 +0100

    first commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Amjad Oudeh on 01.02.22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
