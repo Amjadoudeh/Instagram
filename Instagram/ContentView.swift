07fbeb25edbfda545674d3fdf9eea576c4cc87fb
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-04 00:37:38 +0100

    multi different posts

--- Instagram/ContentView.swift
+++ Instagram/ContentView.swift
@@ -11,11 +11,15 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 0.0) {
             Header()
-            
+            ScrollView(.vertical, showsIndicators: true)
+            {
             Stories()
             Divider()
             Post()
+            Post(image: "amjad2", description: "Viva flamenca")
+            Post(image: "dog", description: "My babay")
             Post(image: "SC", description: "What's next in Music is first at SoundCloud")
+            }
         }
     }
 }
